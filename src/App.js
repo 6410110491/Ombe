1ac6d9adbb8adc323b2463425928268d994e8f50
Poramest  Noosri <6410110307@psu.ac.th>
2024-05-28 16:28:44 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './componants/Home/Home';
 import TopBar from './componants/Navbar/TopBar';
 import Signin from './componants/Signin';
 import Login from './componants/Login';
+import Create_Account from './componants/Create_Account';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/signin" element={<Signin />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/create" element={<Create_Account />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/componants/Started/started_one.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export class started_one extends PureComponent {
+  render() {
+    return (
+      <div>started_one</div>
+    )
+  }
+}
+
+export default started_one
