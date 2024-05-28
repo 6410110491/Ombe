65dc9ddc45f54e6963e1343b5aa95ee6be5e4e1e
Saranphat Sangaun <6410110491@psu.ac.th>
2024-05-28 09:15:02 +0700

    Merge file

--- src/componants/Login.js
+++ src/componants/Login.js
@@ -4,30 +4,32 @@ import { Link } from 'react-router-dom';
 
 function Login() {
     return (
-        <div>
-            <h2>Mornig begins with Obma coffee</h2>
+        <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <div >
+                <h2>Mornig begins with Obma coffee</h2>
 
-            <Button style={{
-                marginTop: '2rem', backgroundColor: "#03764D", border: 'none',
-                borderRadius: '20px', height: '48px', width: '300px', color: '#FFFFFF', fontWeight: 'bold',
-                width: "100%"
-            }}>Login With Email
-            </Button>
+                <Button style={{
+                    marginTop: '2rem', backgroundColor: "#03764D", border: 'none',
+                    borderRadius: '20px', height: '48px', width: '300px', color: '#FFFFFF', fontWeight: 'bold',
+                    width: "100%"
+                }}>Login With Email
+                </Button>
 
-            <Button style={{
-                marginTop: '2rem', backgroundColor: "#386AED", border: 'none',
-                borderRadius: '20px', height: '48px', width: '300px', color: '#FFFFFF', fontWeight: 'bold',
-                width: "100%"
-            }}>Login With Facebook
-            </Button>
-            
+                <Button style={{
+                    marginTop: '2rem', backgroundColor: "#386AED", border: 'none',
+                    borderRadius: '20px', height: '48px', width: '300px', color: '#FFFFFF', fontWeight: 'bold',
+                    width: "100%"
+                }}>Login With Facebook
+                </Button>
 
-            <Button style={{
-                marginTop: '1rem', backgroundColor: "#FFFFFF", 
-                borderRadius: '20px', height: '48px', width: '300px', color: '#000000', fontWeight: 'bold',
-                width: "100%", border:"1px solid #000"
-            }}>Login With Google
-            </Button>
+
+                <Button style={{
+                    marginTop: '1rem', backgroundColor: "#FFFFFF",
+                    borderRadius: '20px', height: '48px', width: '300px', color: '#000000', fontWeight: 'bold',
+                    width: "100%", border: "1px solid #000"
+                }}>Login With Google
+                </Button>
+            </div>
         </div>
 
     )
--- src/componants/Signin.js
+++ src/componants/Signin.js
@@ -22,49 +22,52 @@ function Signin() {
         window.location.href = "/" + path
     }
     return (
-        <div >
-            <form onSubmit={handleLogin} >
-                <h2 style={{ textAlign: 'left' }}>Sign In</h2>
-                <p className="subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+        <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <div >
+                <form onSubmit={handleLogin} >
+                    <h2 style={{ textAlign: 'left' }}>Sign In</h2>
+                    <p className="subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
 
 
-                <div style={{ display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center' }}>
-                    <div className="form-group" style={{width:"100%"}}>
-                        <TextField id="standard-basic" label="Username" variant="standard" type="text" fullWidth
-                            style={{ height: '38px', paddingTop: '0.5rem' }} />
+                    <div style={{ display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center' }}>
+                        <div className="form-group" style={{ width: "100%" }}>
+                            <TextField id="standard-basic" label="Username" variant="standard" type="text" fullWidth
+                                style={{ height: '38px', paddingTop: '0.5rem' }} />
+                        </div>
+                        <div className="form-group" style={{ width: "100%" }}>
+                            <TextField id="standard-basic" label="Password" variant="standard" type="text" fullWidth
+                                style={{ height: '38px', paddingTop: '0.5rem' }} />
+                        </div>
+
+                        <Button style={{
+                            marginTop: '2rem', backgroundColor: "#03764D", border: 'none',
+                            borderRadius: '20px', height: '48px', width: '300px', color: '#FFFFFF', fontWeight: 'bold',
+                            width: "100%"
+                        }}>LOGIN
+                        </Button>
+
                     </div>
-                    <div className="form-group" style={{width:"100%"}}>
-                        <TextField id="standard-basic" label="Password" variant="standard" type="text" fullWidth
-                            style={{ height: '38px', paddingTop: '0.5rem' }} />
-                    </div>
+                    <p className="mb-0 " style={{ fontSize: '14px', marginTop: '1rem' }} >
+                        Forgot password?{" "}
+                        <Link onClick={() => changepage("signup")} style={{ color: "#03764D", textDecoration: "none", fontSize: '14px' }}>
+                            Reset here
+                        </Link>
+                    </p>
 
+                </form>
+                <div className="mt-4" style={{ display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center' }}>
+                    <p className="mb-0  text-center" style={{ fontSize: '14px', marginTop: '2rem', textAlign: 'center' }}>
+                        Don't have an account?{" "}
+                    </p>
                     <Button style={{
-                        marginTop: '2rem', backgroundColor: "#03764D", border: 'none', 
-                        borderRadius: '20px', height: '48px', width: '300px', color: '#FFFFFF', fontWeight: 'bold',
-                        width:"100%"
-                    }}>LOGIN
+                        marginTop: '1rem', backgroundColor: "#F7DBB4", border: 'none', borderRadius: '20px', height: '48px'
+                        , color: '#5B5046', fontWeight: 'bold', width: "100%"
+                    }}>
+                        CREATE AN ACCOUNT
                     </Button>
-
                 </div>
-                <p className="mb-0 " style={{ fontSize: '14px', marginTop: '1rem' }} >
-                    Forgot password?{" "}
-                    <Link onClick={() => changepage("signup")} style={{ color: "#03764D", textDecoration: "none", fontSize: '14px' }}>
-                        Reset here
-                    </Link>
-                </p>
-
-            </form>
-            <div className="mt-4" style={{ display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center' }}>
-                <p className="mb-0  text-center" style={{ fontSize: '14px', marginTop: '2rem', textAlign: 'center' }}>
-                    Don't have an account?{" "}
-                </p>
-                <Button style={{ marginTop: '1rem', backgroundColor: "#F7DBB4", border: 'none', borderRadius: '20px', height: '48px'
-                , color: '#5B5046', fontWeight: 'bold', width:"100%"}}>
-                    CREATE AN ACCOUNT
-                </Button>
             </div>
         </div>
-
 
     )
 }
