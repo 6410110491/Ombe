6a6522bbfa836a4196ad80fdcc508fde6cb56a98
Poramest  Noosri <6410110307@psu.ac.th>
2024-05-28 08:58:12 +0700

    Login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './componants/Home';
+import Signin from './componants/Signin';
+import Login from './componants/Login';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/signin" element={<Signin />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
